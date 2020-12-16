9d7ce915d103b8c1c4d38fb9c3839449b3570e4e
SeoHoJun <ghwns6659@naver.com>
2020-12-16 22:45:57 +0900

    Initial commit

---
+++ src/main/java/Student/Grade_Management/GradeManagementApplication.java
@@ -0,0 +1,13 @@
+package Student.Grade_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Student/Grade_Management/GradeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Student.Grade_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
